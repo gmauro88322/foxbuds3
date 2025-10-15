29190477151b483bb9399b04bd63aaca776746d9
gmauro88322 <guymauro88@gmail.com>
2025-10-15 19:33:53 -0400

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const metadata = { title: "Foxy Buds", description: "Foxy Buds store" };
-import "./globals.css";
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
